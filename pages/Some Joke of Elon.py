91c3a74a62ba88b39d6408b7d54bb22cff172062
Faraz <farazmoin.uk@gmail.com>
2026-05-29 16:10:18 +0530

    Initial Commit5

--- pages/Some Joke of Elon.py
+++ pages/Some Joke of Elon.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+st.title("YF making Fun of ELON!")
+
 st.image("pages/ChatGPT Image May 28, 2026, 01_39_20 PM.png", caption="Poor Elon, i am feeling emotional 🥺")
 st.image("pages/b85ab112-a647-42da-b876-abd5098ef4c4.png", caption="Poor Elon never cries 💪")
 st.image("pages/ea9dea5c-b609-4972-a863-e943181b3e18.png", caption="Poor Elon's Emotional story 🥺")
